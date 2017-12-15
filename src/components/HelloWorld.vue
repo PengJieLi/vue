74025973301cbfd36115a8aa7877d48d07532a9e
PengJieLi <32995176+PengJieLi@users.noreply.github.com>
2017-12-15 10:38:26 +0800

    新建了一个工程

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="con" @click="handleClick"></div>  
+    <div class="con1" @click="handleClick"></div> 
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    methods: {
+      handleClick () {
+        alert(123)
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+  .con{
+    width: 100px;
+    height: 100px;
+    background: red;
+  }
+  .con1{
+    width: 200px;
+    height: 200px;
+    background: green;
+  }
+</style>
